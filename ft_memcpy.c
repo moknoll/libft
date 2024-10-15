19072c3c3864c04673cb3f36673c2a8a3e38768f
moknoll <moritz.knoll@gmx.net>
2024-10-15 12:42:52 +0200

    Add files via upload

---
+++ ft_memcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mknoll <mknoll@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 12:35:13 by mknoll            #+#    #+#             */
+/*   Updated: 2024/10/15 12:42:12 by mknoll           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memcpy(void *dest, const void *src, int n)
+{
+	int				i;
+	unsigned char	*byte_dest;
+	unsigned char	*byte_src;
+
+	i = 0;
+	byte_dest = (unsigned char *) dest;
+	byte_src = (unsigned char *) src;
+	while (i < n)
+	{
+		byte_dest[i] = byte_src[i];
+		i++;
+	}
+	return (dest);
+}

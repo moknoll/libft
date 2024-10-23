db9533d499f81ca3d233e815cce07ff78c2685dc
moknoll <moritz.knoll@gmx.net>
2024-10-23 14:09:52 +0200

    Add files via upload

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,42 +6,42 @@
 /*   By: mknoll <mknoll@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 08:45:52 by moritzknoll       #+#    #+#             */
-/*   Updated: 2024/10/23 10:57:18 by mknoll           ###   ########.fr       */
+/*   Updated: 2024/10/23 14:07:58 by mknoll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-static void	*ft_memset(void *ptr, int value, int n)
-{
-	int				i;
-	unsigned char	*byte_ptr;
+// static void	*ft_memset(void *ptr, int value, int n)
+// {
+// 	int				i;
+// 	unsigned char	*byte_ptr;
 
-	i = 0;
-	byte_ptr = (unsigned char *) ptr;
-	while (i < n)
-	{
-		byte_ptr[i] = (unsigned char) value;
-		i++;
-	}
-	return (ptr);
-}
+// 	i = 0;
+// 	byte_ptr = (unsigned char *) ptr;
+// 	while (i < n)
+// 	{
+// 		byte_ptr[i] = (unsigned char) value;
+// 		i++;
+// 	}
+// 	return (ptr);
+// }
 
-void	*calloc(size_t nitems, size_t size)
-{
-	size_t	total_size;
-	void	*ptr;
+// void	*ft_calloc(size_t nitems, size_t size)
+// {
+// 	size_t	total_size;
+// 	void	*ptr;
 
-	total_size = nitems * size;
-	ptr = (char *)malloc(total_size);
-	if (ptr == NULL)
-	{
-		return (NULL);
-	}
-	ft_memset(ptr, 0, total_size);
-	return (ptr);
-}
+// 	total_size = nitems * size;
+// 	ptr = (char *)malloc(total_size);
+// 	if (ptr == NULL)
+// 	{
+// 		return (NULL);
+// 	}
+// 	ft_memset(ptr, 0, total_size);
+// 	return (ptr);
+// }
 
 static int	ft_digit_count(long int i)
 {
@@ -71,12 +71,12 @@ char	*ft_itoa(int n)
 	i = ft_digit_count(nb);
 	str = malloc(i * sizeof(char) + 1);
 	if (!(str))
-		return (0);
-	str[i--] = 0;
+		return (NULL);
+	str[i--] = '\0';
 	if (nb == 0)
 	{
-		str = ft_calloc(2, sizeof(char));
-		str[0] = 48;
+		str[0] = '0';
+		return (str);
 	}
 	if (nb < 0)
 	{

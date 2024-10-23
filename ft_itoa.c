559c794f30b242d1f736cd6b82b0d7446819f02f
moknoll <moritz.knoll@gmx.net>
2024-10-23 14:24:55 +0200

    Add files via upload

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,42 +6,42 @@
 /*   By: mknoll <mknoll@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 08:45:52 by moritzknoll       #+#    #+#             */
-/*   Updated: 2024/10/23 14:07:58 by mknoll           ###   ########.fr       */
+/*   Updated: 2024/10/23 14:23:24 by mknoll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-// static void	*ft_memset(void *ptr, int value, int n)
-// {
-// 	int				i;
-// 	unsigned char	*byte_ptr;
+static void	*ft_memset(void *ptr, int value, int n)
+{
+	int				i;
+	unsigned char	*byte_ptr;
 
-// 	i = 0;
-// 	byte_ptr = (unsigned char *) ptr;
-// 	while (i < n)
-// 	{
-// 		byte_ptr[i] = (unsigned char) value;
-// 		i++;
-// 	}
-// 	return (ptr);
-// }
+	i = 0;
+	byte_ptr = (unsigned char *) ptr;
+	while (i < n)
+	{
+		byte_ptr[i] = (unsigned char) value;
+		i++;
+	}
+	return (ptr);
+}
 
-// void	*ft_calloc(size_t nitems, size_t size)
-// {
-// 	size_t	total_size;
-// 	void	*ptr;
+void	*ft_calloc(size_t nitems, size_t size)
+{
+	size_t	total_size;
+	void	*ptr;
 
-// 	total_size = nitems * size;
-// 	ptr = (char *)malloc(total_size);
-// 	if (ptr == NULL)
-// 	{
-// 		return (NULL);
-// 	}
-// 	ft_memset(ptr, 0, total_size);
-// 	return (ptr);
-// }
+	total_size = nitems * size;
+	ptr = (char *)malloc(total_size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	ft_memset(ptr, 0, total_size);
+	return (ptr);
+}
 
 static int	ft_digit_count(long int i)
 {
@@ -75,8 +75,8 @@ char	*ft_itoa(int n)
 	str[i--] = '\0';
 	if (nb == 0)
 	{
-		str[0] = '0';
-		return (str);
+		str = ft_calloc(2, sizeof(char));
+		str[0] = 48;
 	}
 	if (nb < 0)
 	{

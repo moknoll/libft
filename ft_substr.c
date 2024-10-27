d4dbe38b80ed16cfe3eba12dc229387ba34784ee
moknoll <moritz.knoll@gmx.net>
2024-10-27 08:45:28 +0100

    Added condition for empty string

--- ft_substr.c
+++ ft_substr.c
@@ -3,17 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mknoll <mknoll@student.42.fr>              +#+  +:+       +#+        */
+/*   By: moritzknoll <moritzknoll@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 10:53:51 by moritzknoll       #+#    #+#             */
-/*   Updated: 2024/10/23 13:01:49 by mknoll           ###   ########.fr       */
+/*   Updated: 2024/10/27 08:44:05 by moritzknoll      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-size_t	ft_strlen(char const *s)
+static char	*ft_strdup(char *s)
+{
+	int		i;
+	int		len;
+	char	*copy;
+
+	i = 0;
+	len = 0;
+	while (s[len])
+	{
+		len++;
+	}
+	copy = (char *) malloc(len + 1);
+	if (copy == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		copy[i] = s[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
+
+static size_t	ft_strlen(char const *s)
 {
 	size_t	i;
 
@@ -31,10 +55,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*sub_string;
 
 	i = 0;
-	if (!s || start >= ft_strlen(s))
+	if (!s)
 	{
 		return (NULL);
 	}
+	if (start > ft_strlen(s))
+        return (ft_strdup(""));
 	if (ft_strlen(s) - start < len)
 	{
 		len = ft_strlen(s) - start;
@@ -51,4 +77,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	}
 	sub_string[len] = '\0';
 	return (sub_string);
+}
+
+int main()
+{
+	char *result = ft_substr("hola", 0, 20);
+	char *result2 = ft_substr("", 0, 1);
+	char *result3 = ft_substr("hola", 4, 20);
+
+	printf("%s\n", result);
+	printf("%s\n", result2);
+	printf("%s\n", result3);
 }

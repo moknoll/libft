8c5909ea18e6fba333a5c47d83bcf515434738af
moknoll <moritz.knoll@gmx.net>
2024-10-28 08:17:55 +0100

    Add files via upload

--- ft_itoa.c
+++ ft_itoa.c
@@ -3,90 +3,99 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mknoll <mknoll@student.42.fr>              +#+  +:+       +#+        */
+/*   By: moritzknoll <moritzknoll@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 08:45:52 by moritzknoll       #+#    #+#             */
-/*   Updated: 2024/10/23 14:23:24 by mknoll           ###   ########.fr       */
+/*   Updated: 2024/10/25 16:07:06 by moritzknoll      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <limits.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-static void	*ft_memset(void *ptr, int value, int n)
-{
-	int				i;
-	unsigned char	*byte_ptr;
-
-	i = 0;
-	byte_ptr = (unsigned char *) ptr;
-	while (i < n)
-	{
-		byte_ptr[i] = (unsigned char) value;
-		i++;
-	}
-	return (ptr);
-}
-
-void	*ft_calloc(size_t nitems, size_t size)
-{
-	size_t	total_size;
-	void	*ptr;
-
-	total_size = nitems * size;
-	ptr = (char *)malloc(total_size);
-	if (ptr == NULL)
-	{
-		return (NULL);
-	}
-	ft_memset(ptr, 0, total_size);
-	return (ptr);
-}
-
-static int	ft_digit_count(long int i)
+static	int counter(long nbr)
 {
 	int	count;
 
 	count = 0;
-	if (i < 0)
+	if (nbr == 0)
+		return 1;
+	if (nbr < 0)
 	{
-		i *= -1;
+		count++;
+		nbr = -nbr;
+	}
+	while (nbr != 0)
+	{
+		nbr /= 10;
 		count++;
 	}
-	while (i > 0)
-	{
-		i /= 10;
-		count++;
-	}
-	return (count);
+    return (count);
 }
 
-char	*ft_itoa(int n)
+static void	ft_bzero(void *s, size_t n)
 {
-	char		*str;
-	int			i;
-	long int	nb;
+	char	*temp_ptr;
 
-	nb = n;
-	i = ft_digit_count(nb);
-	str = malloc(i * sizeof(char) + 1);
-	if (!(str))
+	temp_ptr = (char *) s;
+	while (n > 0)
+	{
+		*(temp_ptr++) = 0;
+		n--;
+	}
+}
+
+static char	*converting(int len)
+{
+	char	*temp;
+	temp = malloc((len + 1) * sizeof(char));
+	if (!temp)
 		return (NULL);
-	str[i--] = '\0';
-	if (nb == 0)
+	ft_bzero(temp, len + 1);
+	return (temp);
+}
+
+char *ft_itoa(int n)
+{
+	int len;
+	int i;
+	char *result;
+	long nbr;
+
+	nbr = n;
+	len = counter(nbr);
+	result = converting(len);
+	if (!result)
+		return (NULL);
+	if (nbr == 0)
+		result[0] = '0';
+	if (nbr < 0)
+		nbr = -nbr;
+	i = len - 1;
+	while (nbr != 0)
 	{
-		str = ft_calloc(2, sizeof(char));
-		str[0] = 48;
+		result[i] = ((nbr % 10) + 48);
+		nbr = nbr / 10;
+		i--;
 	}
-	if (nb < 0)
-	{
-		str[0] = '-';
-		nb = nb * -1;
-	}
-	while (nb > 0)
-	{
-		str[i--] = nb % 10 + '0';
-		nb = nb / 10;
-	}
-	return (str);
+	if (n < 0)
+		result[0] = '-';
+	result[len] = 0;
+	return (result);
+}
+
+int main(void)
+{
+    printf("ft_itoa(123) = %s\n", ft_itoa(123));           // "123"
+    printf("ft_itoa(-456) = %s\n", ft_itoa(-456));         // "-456"
+    printf("ft_itoa(0) = %s\n", ft_itoa(0));               // "0"
+    printf("ft_itoa(INT_MAX) = %s\n", ft_itoa(INT_MAX));   // "2147483647"
+    printf("ft_itoa(INT_MIN) = %s\n", ft_itoa(INT_MIN));   // "-2147483648"
+    printf("ft_itoa(7) = %s\n", ft_itoa(7));               // "7"
+    printf("ft_itoa(-3) = %s\n", ft_itoa(-3));             // "-3"
+    printf("ft_itoa(1000000) = %s\n", ft_itoa(1000000));   // "1000000"
+    printf("ft_itoa(-1000000) = %s\n", ft_itoa(-1000000)); // "-1000000"
+
+    return 0;
 }
